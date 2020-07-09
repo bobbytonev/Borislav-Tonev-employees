0038ba0edfbbcdba37cfc89aa21dc0965ad5bc1d
Bobby Tonev <btonev@tu-sofia.bg>
2020-07-10 00:45:14 +0300

    s

---
+++ Company/src/NotValidDateException.java
@@ -0,0 +1,9 @@
+
+public class NotValidDateException extends Exception{
+
+	 public NotValidDateException() { super(); }
+	  public NotValidDateException(String message) { super(message); }
+	  public NotValidDateException(String message, Throwable cause) { super(message, cause); }
+	  public NotValidDateException(Throwable cause) { super(cause); }
+
+}
